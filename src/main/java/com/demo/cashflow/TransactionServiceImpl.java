9dd67c366061845d0428553587c93725e4296c2c
air <kobychev00@bk.ru>
2025-02-13 22:33:46 +0300

    add outcome processing to getTransactionById

--- src/main/java/com/demo/cashflow/TransactionServiceImpl.java
+++ src/main/java/com/demo/cashflow/TransactionServiceImpl.java
@@ -1,6 +1,7 @@
 package com.demo.cashflow;
 
 import com.demo.cashflow.domain.Income;
+import com.demo.cashflow.domain.Outcome;
 import com.demo.cashflow.domain.Transaction;
 import org.springframework.stereotype.Service;
 
@@ -13,21 +14,28 @@ import java.util.Map;
 public class TransactionServiceImpl implements TransactionService {
 
     Map<String, Transaction> transactions = new HashMap<>(Map.of(
-            "0001", new Income(new GregorianCalendar(2025, Calendar.JANUARY, 25), 1, 15500, 1),
-            "0002", new Income(new GregorianCalendar(2025, Calendar.JANUARY, 31), 1, 10000, 3)
+            "0001", new Income(new GregorianCalendar(2025, Calendar.JANUARY, 25), 1, 65000, 1),
+            "0002", new Income(new GregorianCalendar(2025, Calendar.JANUARY, 31), 1, 8000, 2),
+            "0003", new Outcome(new GregorianCalendar(2025,Calendar.JANUARY,31),1,10000,1)
     ));
 
     Map <Integer, String> types = Map.of(
             1, "Income",
-            2, "Outcome"
-    );
+            2, "Outcome");
 
     Map <Integer, String> sources = Map.of(
             1, "Technopark",
-            2, "Detvora",
-            3, "Digital4Food",
-            4, "BKI"
-    );
+            2, "Detvora");
+
+    Map<Integer, String> categories = Map.of(
+            1, "Accomodation",
+            2, "Food",
+            3, "Transport",
+            4, "Commutication",
+            5, "Recration",
+            6, "Subcriptions",
+            7, "Presents",
+            8, "Savings" );
 
     @Override
     public String getTransactionById(String id) {
@@ -45,7 +53,7 @@ public class TransactionServiceImpl implements TransactionService {
         // Создаем таблицу с заголовками
         StringBuilder transactionDescription = new StringBuilder();
         transactionDescription.append("<table border='1' style='border-collapse: collapse;'>")
-                .append("<tr><th>Date</th><th>Type</th><th>Amount</th><th>Source</th></tr>")
+                .append("<tr><th>Date</th><th>Type</th><th>Amount</th><th>Source</th><th>Category</th></tr>")
                 .append("<tr>")
                 .append("<td>").append(formattedDate).append("</td>")
                 .append("<td>").append(typeName).append("</td>")
@@ -69,12 +77,33 @@ public class TransactionServiceImpl implements TransactionService {
                 sourcesList.setLength(sourcesList.length() - 2);
                 sourceText = sourcesList.toString();
             }
+            //Cоздание переменной для записи категории (передача значения по умолачанию в таблицу для Income)
+            String categoryText = "-";
+            // Добавляем значение источника в таблицу
+            transactionDescription.append("<td>" ).append(sourceText).append("</td>" )
+                    .append("<td>").append(categoryText).append("</td>");
         }
+        String categoryText = "-";
+        //Проверяем является ли транзакция Outcome
+        if (transaction instanceof Outcome) {
+            Outcome outcome = (Outcome) transaction;
+            StringBuilder categoriesList = new StringBuilder();
+            for (Integer outcomeNumber : outcome.getCategoryNumbers()) {
+                String outcomeName = categories.get(outcomeNumber);
+                if (outcomeName != null) {
+                    categoriesList.append(outcomeName).append(", " );
+                }
+            }
 
-        // Добавляем значение источника в таблицу
-        transactionDescription.append("<td>").append(sourceText).append("</td>")
-                .append("</tr></table>");
-
+            //Убираем лишнюю запятую и пробел
+            if (categoriesList.length() > 0) {
+                categoriesList.setLength(categoriesList.length() - 2);
+                categoryText = categoriesList.toString();
+            }
+            //Добавляем значение категории в таблицу
+            transactionDescription.append("<td>" ).append(sourceText).append("</td>" )
+                    .append("<td>").append(categoryText).append("</td>");
+        }
         return transactionDescription.toString();
     }
 
